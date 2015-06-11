7d67bde95f7c5d0f0da8644ef316cc0301d08d65
lsdlab <lsdlab@icloud.com>
2015-06-12 00:32:19 +0800

    commit

--- article/admin.py
+++ article/admin.py
@@ -32,9 +32,10 @@ from article.models import *
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'publish_time')
     search_fields = (
-        'title', 'category', 'tag', 'publish_time', 'update_time' 'content')
+        'title', 'category', 'tag', 'content')
     list_filter = ('publish_time',)
     date_hierarchy = 'publish_time'
+
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Tag)
